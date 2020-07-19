665cace11aa841d1bfeb5d667ddb0a16d74a8584
siwel20 <plthrock@gmail.com>
2020-07-19 16:33:07 -0600

    added delete function and added function to track note ids

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,10 +1,18 @@
 const db = require("../db/db.json");
 const fs = require("fs");
+
+let id = db.length + 1;
 function apiRoutes(app) {
    app.get("/api/notes", (req, res) => {
        res.json(db);
-   })
+   });
    app.post("/api/notes", (req, res) => {
+       req.body.id = id++;
+       db.forEach(db => {
+           if(req.body.db === db.id) {
+               req.body.id++
+           }
+       })
        db.push(req.body)
        fs.writeFile("./db/db.json", JSON.stringify(db), function(error) {
            if (error) {
@@ -12,7 +20,18 @@ function apiRoutes(app) {
            }
            res.json(db);
        });
-   })
+   });
+   app.delete("/api/notes/:id", (req, res) => {
+       for (let i=0; i<db.length; i++) {
+           if(db[i].id === parseInt(req.params.id)) {
+               db.splice(i, 1)
+           }
+       }
+       fs.writeFile("./db/db.json", JSON.stringify(db), function(error) {
+           if (error) throw error;
+           res.json(db);
+       });
+   });
 }
 
 module.exports = apiRoutes; 
